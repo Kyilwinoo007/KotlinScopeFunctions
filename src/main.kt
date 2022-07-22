10b1c91c223c98b4d4c8f732a9a4fa4a005323ce
ko kyi <kokyi6171@gmail.com>
2022-07-22 16:24:10 +0630

    add comment

--- src/main.kt
+++ src/main.kt
@@ -10,33 +10,65 @@ fun main(args:Array<String>){
 		if(firstPerson.age >= secondPerson.age) firstPerson else secondPerson
 	}.printPerson()
 	
+	/** Scope Function:'with'
+		Property 1 :Refer to context object using 'this'
+		Property 2 :The return value is the 'lambda result' **/
 	with(firstPerson){
 		age += 1
-		"Age is now $age"
+		"Age is now $age"   //return String
 	}.println()
 	
-	firstPerson.run{
-		age += 1
-		"Age is now $age"
-	}.println()
-	
-	firstPerson.let{
-		modifyPerson ->
-		modifyPerson.age += 1
-		"Age is now ${modifyPerson.age}"
-	}.println()
-	
-	firstPerson.printPerson()
-	
+	/** Scope Function: 'apply'
+		Property 1 :Refer to context object using 'this'
+		Property 2 :The return value is the 'context object' **/
 	secondPerson.apply{
 		age += 1
 		job = "Software Tester"
 	}.printPerson()
 	
-	secondPerson.also{
+	
+	/** Scope Function: 'also'
+		Property 1 :Refer to context object using 'it'
+		Property 2 :The return value is the 'context object' **/
+	
+	secondPerson.also{   
 		it.age += 1
 		it.job = "Manager"
 	}.printPerson()
+	
+	
+	/** Scope Function: 'let'
+		Property 1 :Refer to context object using 'it'
+		Property 2 :The return value is the 'lambda result' **/
+	
+	//Use 'let' function to avoid NullPointerException
+	
+	firstPerson.let{
+		modifyPerson ->
+		modifyPerson.age += 1
+		"Age is now ${modifyPerson.age}"  //return String
+	}.println()
+	
+	val name:String? = null
+	name?.let{
+		println(it.reversed());
+		println(it.capitalize())
+		println(it.length)
+	}
+	
+	
+	
+	/** Scope Function: 'run'
+		Property 1 :Refer to context object using 'this'
+		Property 2 :The return value is the 'lambda result' **/
+	
+	
+	firstPerson.run{
+		age += 1
+		"Age is now $age"   //return String
+	}.println()
+	
+		
 }
 
 data class Person(var name:String,var age:Int,var job:String){
